4e2f27c1330bdfd34cad2508a6130f56c38c0a4f
Angelica <acastilloto@ulasalle.edu.pe>
2025-12-03 22:52:30 -0500

    creando carpetas para arquitectura MVVC

---
+++ app/src/main/java/com/example/flexapp/model/QuoteModel.kt
@@ -0,0 +1,3 @@
+package com.example.flexapp.model
+
+data class QuoteModel (val quote: String, val message: String, val author: String)
---
+++ app/src/main/java/com/example/flexapp/model/QuoteProvider.kt
@@ -0,0 +1,11 @@
+package com.example.flexapp.model
+import com.example.flexapp.model.QuoteModel
+
+class QuoteProvider {
+    val quote = listOf(
+        QuoteModel(
+            quote = "Gestiona tus envíos de manera simple",
+            author = "Organiza pedidos diarios y mantén todo bajo control, sin complicaciones."
+        )
+    )
+}
